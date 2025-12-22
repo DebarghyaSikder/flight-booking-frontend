dc7649051167cfb1aeb7129c739d59d9c244ffe1
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-22 19:18:46 +0530

    Initial commit

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  private baseUrl = environment.apiUrl + '/auth';
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, data);
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, data);
+  }
+}
---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class TokenService {
+
+  private TOKEN_KEY = 'jwt_token';
+
+  save(token: string) {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  get(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  clear() {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.get();
+  }
+}
